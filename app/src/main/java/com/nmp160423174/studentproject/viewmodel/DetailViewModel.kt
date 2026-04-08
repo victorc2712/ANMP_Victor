c14f4f8332281fab59fa7e0c346efb7a05743fec
victorc2712 <victorchristian2712@gmail.com>
2026-04-08 08:38:39 +0700

    Modify DetailViewModel.kt, add error handling

--- app/src/main/java/com/nmp160423174/studentproject/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/nmp160423174/studentproject/viewmodel/DetailViewModel.kt
@@ -1,6 +1,7 @@
 package com.nmp160423174.studentproject.viewmodel
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -12,14 +13,19 @@ import com.nmp160423174.studentproject.model.Student
 
 class DetailViewModel(application: Application): AndroidViewModel(application) {
     val studentLD = MutableLiveData<Student>()
+    val errorLD = MutableLiveData<Boolean>()
     val TAG:String = "Volley Tag"
     var queue: RequestQueue?=null
 
     fun fetch(student: Student) {
         queue = Volley.newRequestQueue(getApplication())
         val url = "https://www.jsonkeeper.com/b/LLMW"
+        errorLD.value = false
 
-        val stringRequest = StringRequest(Request.Method.GET, url, {}, {})
+        val stringRequest = StringRequest(Request.Method.GET, url, {}, {
+            Log.d("volley_status", it.message.toString())
+            errorLD.value = true
+        })
         stringRequest.tag = TAG
         queue?.add(stringRequest)
 
